711b542abb96708d430749968e15b64f5b7d276b
Marcus Ekwall <marcus.ekwall@animech.com>
2020-03-03 16:34:46 +0100

    refactor: getAccessorTypeSize

--- src/lib/gltf/utils/getAccessorTypeSize.ts
+++ src/lib/gltf/utils/getAccessorTypeSize.ts
@@ -1,19 +1,13 @@
+const accessorSizes: Record<string, number> = {
+  SCALAR: 1,
+  VEC2: 2,
+  VEC3: 3,
+  VEC4: 4,
+  MAT2: 4,
+  MAT3: 9,
+  MAT4: 16,
+};
+
 export function getAccessorTypeSize(type: string) {
-  switch (type) {
-    case "SCALAR":
-      return 1;
-    case "VEC2":
-      return 2;
-    case "VEC3":
-    default:
-      return 3;
-    case "VEC4":
-      return 4;
-    case "MAT2":
-      return 4;
-    case "MAT3":
-      return 9;
-    case "MAT4":
-      return 16;
-  }
+  return accessorSizes[type] ?? 3;
 }
