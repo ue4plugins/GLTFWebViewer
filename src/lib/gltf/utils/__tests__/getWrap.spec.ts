88e828492236876e7f40470d99e2765ff6543ad2
Marcus Ekwall <marcus.ekwall@animech.com>
2020-03-03 17:21:07 +0100

    test: add spec for getWrap

---
+++ src/lib/gltf/utils/__tests__/getWrap.spec.ts
@@ -0,0 +1,20 @@
+import "jest";
+import { getWrap } from "../getWrap";
+
+describe("getWrap", () => {
+  it("should get pc.ADDRESS_REPEAT as default", () => {
+    expect(getWrap(1337)).toBe(pc.ADDRESS_REPEAT);
+  });
+
+  it("should get pc.ADDRESS_CLAMP_TO_EDGE", () => {
+    expect(getWrap(33071)).toBe(pc.ADDRESS_CLAMP_TO_EDGE);
+  });
+
+  it("should get pc.ADDRESS_MIRRORED_REPEAT", () => {
+    expect(getWrap(33648)).toBe(pc.ADDRESS_MIRRORED_REPEAT);
+  });
+
+  it("should get pc.ADDRESS_REPEAT", () => {
+    expect(getWrap(10497)).toBe(pc.ADDRESS_REPEAT);
+  });
+});
