e50d6c70f415f09f1541e011d4a17dfa633a4642
Marcus Ekwall <marcus.ekwall@animech.com>
2020-03-05 17:39:23 +0100

    refactor(translateTexture): type arguments

--- src/lib/gltf/utils/translateTexture.ts
+++ src/lib/gltf/utils/translateTexture.ts
@@ -1,15 +1,20 @@
 import pc from "playcanvas";
 import createDebug from "debug";
 import { GlTfParser } from "../GlTfParser";
-import { Texture } from "../types";
+import { Texture, GlTf } from "../types";
 import { getFilter } from "./getFilter";
 import { getWrap } from "./getWrap";
 
 const debug = createDebug("translateTexture");
 
+interface Arguments {
+  gltf: GlTf;
+  device: pc.GraphicsDevice;
+}
+
 // Specification:
 //   https://github.com/KhronosGroup/glTF/tree/master/specification/2.0#texture
-export function translateTexture(data: Texture, { gltf, device }: GlTfParser) {
+export function translateTexture(data: Texture, { gltf, device }: Arguments) {
   debug("begin");
   const texture = new pc.Texture(device, {
     flipY: false,
