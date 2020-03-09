8b17f743584355f444bee78ed6278ef2e9b15632
Marcus Ekwall <marcus.ekwall@animech.com>
2020-03-09 16:47:25 +0100

    fix(shouldResampleImage): broken sanity check

--- src/lib/gltf/utils/shouldResampleImage.ts
+++ src/lib/gltf/utils/shouldResampleImage.ts
@@ -10,8 +10,8 @@ const minmapValues = [
 ];
 
 export function shouldResampleImage(aU?: number, aV?: number, mF?: number) {
-  const isAURepeat = aU && repeatValues.includes(aU);
-  const isAVRepeat = aV && repeatValues.includes(aV);
-  const nearestMinFilter = mF && minmapValues.includes(mF);
+  const isAURepeat = repeatValues.includes(aU ?? -1);
+  const isAVRepeat = repeatValues.includes(aV ?? -1);
+  const nearestMinFilter = minmapValues.includes(mF ?? -1);
   return !!(isAURepeat || isAVRepeat || nearestMinFilter);
 }
