c615f5b7beaf15933895284282d3b0a484f75828
Marcus Ekwall <marcus.ekwall@animech.com>
2020-03-09 16:50:34 +0100

    test(shouldResampleImage): cover additional branches

--- src/lib/gltf/utils/__tests__/shouldResampleImage.spec.ts
+++ src/lib/gltf/utils/__tests__/shouldResampleImage.spec.ts
@@ -4,26 +4,30 @@ import { shouldResampleImage } from "../shouldResampleImage";
 
 describe("shouldResampleImage", () => {
   it("should return correct for adressU", () => {
-    expect(shouldResampleImage(pc.ADDRESS_REPEAT, -1, -1)).toBe(true);
-    expect(shouldResampleImage(pc.ADDRESS_MIRRORED_REPEAT, -1, -1)).toBe(true);
+    let ret = shouldResampleImage(pc.ADDRESS_REPEAT, undefined, -1);
+    expect(ret).toBe(true);
+    ret = shouldResampleImage(pc.ADDRESS_MIRRORED_REPEAT, -1, undefined);
+    expect(ret).toBe(true);
     expect(shouldResampleImage(1337, -1, -1)).toBe(false);
   });
 
   it("should return correct for adressV", () => {
-    expect(shouldResampleImage(-1, pc.ADDRESS_REPEAT, -1)).toBe(true);
-    expect(shouldResampleImage(-1, pc.ADDRESS_MIRRORED_REPEAT, -1)).toBe(true);
+    let ret = shouldResampleImage(-1, pc.ADDRESS_REPEAT, undefined);
+    expect(ret).toBe(true);
+    ret = shouldResampleImage(undefined, pc.ADDRESS_MIRRORED_REPEAT, -1);
+    expect(ret).toBe(true);
     expect(shouldResampleImage(-1, 1337, -1)).toBe(false);
   });
 
   it("should return correct for minFilter", () => {
-    let ret = shouldResampleImage(-1, -1, pc.FILTER_LINEAR_MIPMAP_LINEAR);
+    let ret = shouldResampleImage(null, -1, pc.FILTER_LINEAR_MIPMAP_LINEAR);
     expect(ret).toBe(true);
-    ret = shouldResampleImage(-1, -1, pc.FILTER_NEAREST_MIPMAP_LINEAR);
+    ret = shouldResampleImage(-1, null, pc.FILTER_NEAREST_MIPMAP_LINEAR);
     expect(ret).toBe(true);
-    ret = shouldResampleImage(-1, -1, pc.FILTER_LINEAR_MIPMAP_NEAREST);
+    ret = shouldResampleImage(null, -1, pc.FILTER_LINEAR_MIPMAP_NEAREST);
     expect(ret).toBe(true);
-    ret = shouldResampleImage(-1, -1, pc.FILTER_NEAREST_MIPMAP_NEAREST);
+    ret = shouldResampleImage(-1, null, pc.FILTER_NEAREST_MIPMAP_NEAREST);
     expect(ret).toBe(true);
-    expect(shouldResampleImage(-1, -1, 1337)).toBe(false);
+    expect(shouldResampleImage(null, -1, 1337)).toBe(false);
   });
 });
