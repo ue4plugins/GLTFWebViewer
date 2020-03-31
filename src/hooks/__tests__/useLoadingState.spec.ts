8e274c3dd3fc98416236438fe550461617f0fc02
Samuel Johansson <samuel.johansson@animech.com>
2020-03-31 11:00:58 +0200

    test: add tests for useLoadingState hook

---
+++ src/hooks/__tests__/useLoadingState.spec.ts
@@ -0,0 +1,56 @@
+import "jest";
+import { renderHook, act } from "@testing-library/react-hooks";
+import { useLoadingState } from "../useLoadingState";
+
+describe("useLoadingState", () => {
+  it("should not be loading when initially", async () => {
+    const { result } = renderHook(() => useLoadingState());
+    expect(result.current[0]).toBe(false);
+  });
+
+  it("should start loading when a task starts", async () => {
+    const { result } = renderHook(() => useLoadingState());
+    act(() => {
+      const [_, start] = result.current;
+      start();
+    });
+    expect(result.current[0]).toBe(true);
+  });
+
+  it("should stop loading when all tasks have ended", async () => {
+    const { result } = renderHook(() => useLoadingState());
+    act(() => {
+      const [_, start, end] = result.current;
+      start();
+      end();
+    });
+    expect(result.current[0]).toBe(false);
+  });
+
+  it("should not stop loading when if some task is still ongoing", async () => {
+    const { result } = renderHook(() => useLoadingState());
+    act(() => {
+      const [_, start, end] = result.current;
+      start();
+      start();
+      end();
+    });
+    expect(result.current[0]).toBe(true);
+    act(() => {
+      const [_, __, end] = result.current;
+      end();
+    });
+    expect(result.current[0]).toBe(false);
+  });
+
+  it("should use memoized callback refs", async () => {
+    const { result } = renderHook(() => useLoadingState());
+    const [_, startRef, endRef] = result.current;
+    act(() => {
+      // Trigger render
+      result.current[1]();
+    });
+    expect(startRef).toBe(result.current[1]);
+    expect(endRef).toBe(result.current[2]);
+  });
+});
