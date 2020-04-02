e90931cb4eadd1bc55ba8912222b89b652834eab
Samuel Johansson <samuel.johansson@animech.com>
2020-04-02 15:57:59 +0200

    chore: remove unused husky config

--- husky.config.js
+++ husky.config.js
@@ -1,7 +1,5 @@
 module.exports = {
   hooks: {
     "pre-commit": "lint-staged",
-    // "pre-push": "npx tsc -p tsconfig.json --noEmit && npm run test",
-    // "commit-msg": "commitlint -E HUSKY_GIT_PARAMS",
   },
 };
