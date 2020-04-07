9da228749b5f9757f55b9e3512f11fa309db1440
Samuel Johansson <samuel.johansson@animech.com>
2020-04-07 19:24:41 +0200

    refactor(PlayCanvasViewer): remove side-effects from createApp

--- src/lib/PlayCanvasViewer.ts
+++ src/lib/PlayCanvasViewer.ts
@@ -19,32 +19,24 @@ type OrbitCameraEntity = pc.Entity & {
 };
 
 export class PlayCanvasViewer {
-  public app: pc.Application;
-  public camera!: OrbitCameraEntity;
-  public playing = true;
-  public gltf?: pc.Entity;
-  public model?: pc.Asset;
-  public asset?: pc.Asset;
-  public scene?: pc.Scene;
-  public textures: pc.Asset[] = [];
-  public animations: pc.Asset[] = [];
+  private app: pc.Application;
+  private camera: OrbitCameraEntity;
+  private gltf?: pc.Entity;
+  private model?: pc.Asset;
+  private asset?: pc.Asset;
+  private scene?: pc.Scene;
+  private textures: pc.Asset[] = [];
+  private animations: pc.Asset[] = [];
   private debouncedCanvasResize = debounce(() => this.resizeCanvas(), 10);
   private canvasResizeObserver = new ResizeObserver(this.debouncedCanvasResize);
 
   public constructor(public canvas: HTMLCanvasElement) {
-    if (!canvas) {
-      throw new Error("Missing canvas");
-    }
     this.resizeCanvas = this.resizeCanvas.bind(this);
-    // TODO: remove side effects of createApp
-    this.app = this.createApp();
-  }
 
-  private resizeCanvas() {
-    this.app.resizeCanvas(
-      this.canvas.parentElement?.clientWidth,
-      this.canvas.parentElement?.clientHeight,
-    );
+    this.app = this.createApp();
+    this.camera = this.createCamera(this.app);
+
+    this.canvasResizeObserver.observe(this.canvas);
   }
 
   public get isReady() {
@@ -56,14 +48,21 @@ export class PlayCanvasViewer {
     return (this.app as any)._sceneRegistry?.list() || [];
   }
 
+  private resizeCanvas() {
+    this.app.resizeCanvas(
+      this.canvas.parentElement?.clientWidth,
+      this.canvas.parentElement?.clientHeight,
+    );
+  }
+
   private createApp() {
     const existingApp = pc.Application.getApplication();
     if (existingApp) {
-      debug("Destroying existing PlayCanvas app");
+      debug("Destroying existing app");
       existingApp.destroy();
     }
 
-    debug("Creating PlayCanvas for target", this.canvas);
+    debug("Creating app for target", this.canvas);
     const app = new pc.Application(this.canvas, {
       assetPrefix: "assets/playcanvas/",
       mouse: new pc.Mouse(document.body),
@@ -77,15 +76,6 @@ export class PlayCanvasViewer {
       },
     });
 
-    // TODO: refactor and make scripts strongly typed
-    createCameraScripts(app);
-
-    // TODO: remove?
-    const Rotate = pc.createScript("rotate");
-    Rotate.prototype.update = function(deltaTime: number) {
-      this.entity.rotate(0, deltaTime * 20, 0);
-    };
-
     app.setCanvasFillMode(pc.FILLMODE_NONE);
     app.setCanvasResolution(pc.RESOLUTION_AUTO);
     app.resizeCanvas(
@@ -93,28 +83,36 @@ export class PlayCanvasViewer {
       this.canvas.parentElement?.clientHeight,
     );
 
-    this.canvasResizeObserver.observe(this.canvas);
+    debug("Starting app");
+    app.start();
 
+    return app;
+  }
+
+  private createCamera(app: pc.Application) {
     debug("Creating camera");
-    const camera = new pc.Entity("camera");
+
+    // TODO: refactor and make scripts strongly typed
+    createCameraScripts(app);
+
+    const camera = new pc.Entity("camera") as OrbitCameraEntity;
     camera.addComponent("camera", {
       fov: 45.8366,
       clearColor: new pc.Color(0, 0, 0),
     });
+
     camera.setPosition(0, 0, 8);
+
     camera.addComponent("script");
     if (camera.script) {
       camera.script.create("orbitCamera");
       camera.script.create("keyboardInput");
       camera.script.create("mouseInput");
     }
+
     app.root.addChild(camera);
-    this.camera = camera as OrbitCameraEntity;
 
-    debug("Starting app");
-    app.start();
-
-    return app;
+    return camera;
   }
 
   public destroy() {
@@ -221,7 +219,6 @@ export class PlayCanvasViewer {
       shadowType: pc.SHADOW_VSM32,
     });
     this.gltf.addComponent("script");
-    // this.gltf.script?.create("rotate");
     this.app.root.addChild(this.gltf);
 
     debug("Init animations", this.animations);
