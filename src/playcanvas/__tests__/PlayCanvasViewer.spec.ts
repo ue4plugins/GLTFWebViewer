549cb7a7f6c1aefa0bad212ed234ff5a3ed9b178
Samuel Johansson <samuel.johansson@animech.com>
2020-04-14 17:16:09 +0200

    test: update test description

--- src/playcanvas/__tests__/PlayCanvasViewer.spec.ts
+++ src/playcanvas/__tests__/PlayCanvasViewer.spec.ts
@@ -2,7 +2,7 @@ import "jest";
 import { PlayCanvasViewer } from "../PlayCanvasViewer";
 
 describe("PlayCanvasViewer", () => {
-  it("should start", () => {
+  it("should start without crashing", () => {
     const canvas = document.createElement("canvas");
     const viewer = new PlayCanvasViewer(canvas);
     expect(viewer.configure).toBeDefined();
