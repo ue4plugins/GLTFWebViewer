0c043b2c6fcb507e28d8b3548935b6fef12b184c
Samuel Johansson <samuel.johansson@animech.com>
2020-04-14 17:31:54 +0200

    test: refactor snapshot test

--- e2e/screenshots.test.ts
+++ e2e/screenshots.test.ts
@@ -18,47 +18,44 @@ const waitUntilTrue = async (
   });
 };
 
+const waitForViewer = (timeout = 10000) =>
+  waitUntilTrue(() => page.evaluate(() => !!window.viewerInitiated), timeout);
+
+const waitForScene = (timeout = 10000) =>
+  waitUntilTrue(() => page.evaluate(() => !!window.viewerSceneLoaded), timeout);
+
+const waitForModel = (timeout = 10000) =>
+  waitUntilTrue(() => page.evaluate(() => !!window.viewerModelLoaded), timeout);
+
 describe("todo", () => {
   it("todo", async () => {
     await page.goto("http://localhost:3001");
     await page.setViewport({ width: 1920, height: 1080 });
 
-    await Promise.all([
-      waitUntilTrue(() => page.evaluate(() => !!window.viewerInitiated)),
-      waitUntilTrue(() => page.evaluate(() => !!window.viewerSceneLoaded)),
-      waitUntilTrue(() => page.evaluate(() => !!window.viewerModelLoaded)),
-    ]);
+    await Promise.all([waitForViewer(), waitForScene(), waitForModel()]);
 
     await page.click("#search-input");
     expect(await page.screenshot()).toMatchImageSnapshot();
 
-    // await expect(page).toFill("#search-input", "Duck");
-    // await page.waitFor(1000);
-    // expect(await page.screenshot()).toMatchImageSnapshot();
+    await expect(page).toFill("#search-input", "Duck");
+    expect(await page.screenshot()).toMatchImageSnapshot();
 
-    // const firstItem = (await page.$$("#model-list .MuiListItem-button"))[3];
-    // if (!firstItem) {
-    //   throw new Error("Missing item");
-    // }
+    const firstItem = (await page.$$("#model-list .MuiListItem-button"))[3];
+    if (!firstItem) {
+      throw new Error("Missing item");
+    }
 
-    // const firstItemSpan = await firstItem.$(".MuiListItemText-primary");
-    // if (!firstItemSpan) {
-    //   throw new Error("Missing item span");
-    // }
+    const firstItemSpan = await firstItem.$(".MuiListItemText-primary");
+    if (!firstItemSpan) {
+      throw new Error("Missing item span");
+    }
 
-    // expect(
-    //   await page.evaluate(element => element.textContent, firstItemSpan),
-    // ).toBe("Duck");
+    expect(
+      await page.evaluate(element => element.textContent, firstItemSpan),
+    ).toBe("Duck");
 
-    // await firstItem.hover();
-    // expect(await page.screenshot()).toMatchImageSnapshot();
-
-    // try {
-    //   await firstItem.click();
-    // } catch (e) {
-    //   console.error("Failed to click item", e);
-    // }
-    // await page.waitFor(1000);
-    // expect(await page.screenshot()).toMatchImageSnapshot();
+    await firstItem.click();
+    await waitForModel(3000);
+    expect(await page.screenshot()).toMatchImageSnapshot();
   });
 });
