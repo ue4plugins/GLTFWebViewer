0768066fa0f8d5feb4e0acebada0bad47c0ec613
Samuel Johansson <samuel.johansson@animech.com>
2020-04-15 14:09:51 +0200

    feat: improve way of selecting model via url param

--- src/stores/ModelStore.ts
+++ src/stores/ModelStore.ts
@@ -3,6 +3,7 @@ import { GltfFile } from "../playcanvas";
 
 const urlParams = new URLSearchParams(window.location.search);
 const defaultModel = urlParams.get("model");
+const defaultModelType = urlParams.get("modelType");
 
 export class ModelStore {
   @observable
@@ -28,11 +29,16 @@ export class ModelStore {
     this.models = models;
 
     if (!this.model) {
-      this.model = this.models.find(
-        m =>
-          m.name === defaultModel ||
-          (m.name === "DamagedHelmet" && m.type === "unpacked"), // TODO: remove this line
-      );
+      this.model =
+        models.length > 0
+          ? defaultModel
+            ? models.find(
+                m =>
+                  m.name === defaultModel &&
+                  (m.type === defaultModelType || defaultModelType === null),
+              )
+            : models[0]
+          : undefined;
     }
   }
 }
