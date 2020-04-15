7e83ff2146ff04b52b3e386a66becf4032099679
Samuel Johansson <samuel.johansson@animech.com>
2020-04-15 16:25:36 +0200

    test(e2e): use page.waitForFunction instead of custom function for viewer waiters

--- e2e/lib/waiters.ts
+++ e2e/lib/waiters.ts
@@ -1,26 +1,17 @@
-export const waitUntilTrue = async (
-  expression: () => Promise<boolean>,
-  timeout = 10000,
-) => {
-  const interval = 500;
-  if (timeout < interval) {
-    return Promise.reject(new Error("waitUntilTrue timed out"));
-  }
-  if (await expression()) {
-    return Promise.resolve();
-  }
-  return new Promise(resolve => {
-    setTimeout(() => {
-      resolve(waitUntilTrue(expression, timeout - interval));
-    }, interval);
-  });
-};
-
 export const waitForViewer = (timeout = 10000) =>
-  waitUntilTrue(() => page.evaluate(() => !!window.viewerInitiated), timeout);
+  page.waitForFunction(() => !!window.viewerInitiated, {
+    polling: 500,
+    timeout,
+  });
 
 export const waitForScene = (timeout = 10000) =>
-  waitUntilTrue(() => page.evaluate(() => !!window.viewerSceneLoaded), timeout);
+  page.waitForFunction(() => !!window.viewerSceneLoaded, {
+    polling: 500,
+    timeout,
+  });
 
 export const waitForModel = (timeout = 10000) =>
-  waitUntilTrue(() => page.evaluate(() => !!window.viewerModelLoaded), timeout);
+  page.waitForFunction(() => !!window.viewerModelLoaded, {
+    polling: 500,
+    timeout,
+  });
