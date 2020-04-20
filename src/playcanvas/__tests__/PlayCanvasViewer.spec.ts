2dd92de4ffd6350df9243387792ba5c8d0a35f1e
Samuel Johansson <samuel.johansson@animech.com>
2020-04-20 11:15:57 +0200

    test(PlayCanvasViewer): fix error

--- src/playcanvas/__tests__/PlayCanvasViewer.spec.ts
+++ src/playcanvas/__tests__/PlayCanvasViewer.spec.ts
@@ -4,7 +4,7 @@ import { PlayCanvasViewer } from "../PlayCanvasViewer";
 describe("PlayCanvasViewer", () => {
   it("should start without crashing", () => {
     const canvas = document.createElement("canvas");
-    const viewer = new PlayCanvasViewer(canvas);
+    const viewer = new PlayCanvasViewer(canvas, { autoPlayAnimations: false });
     expect(viewer.configure).toBeDefined();
   });
 });
