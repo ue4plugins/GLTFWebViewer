9a04a1a2369dc82d730c2ee39b5ce70d943bf84d
Samuel Johansson <samuel.johansson@animech.com>
2020-04-24 17:01:58 +0200

    chore: remove now irrelevant comment [skip ci]

--- e2e/jest.config.js
+++ e2e/jest.config.js
@@ -5,9 +5,6 @@ module.exports = {
   testMatch: ["**/e2e/?(*.)+(spec|test).[t]s"],
   testPathIgnorePatterns: ["<rootDir>/build/", "<rootDir>/node_modules/"],
   setupFilesAfterEnv: ["<rootDir>/setupTests.ts"],
-  // TODO: Add reporter for persistent snapshot storage, see example:
-  // https://github.com/americanexpress/jest-image-snapshot#upload-diff-images-from-failed-tests
-  // reporters: ["default", "<rootDir>/image-reporter.js"],
   transform: {
     "^.+\\.ts?$": "ts-jest",
   },
