b38d1b5175f1c8fea9da6e48645cf041035925c6
Samuel Johansson <samuel.johansson@animech.com>
2020-05-04 12:11:55 +0200

    refactor(useModelDrop): move utility functions to separate files

--- src/hooks/useModelDrop.ts
+++ src/hooks/useModelDrop.ts
@@ -2,70 +2,10 @@ import path from "path";
 import { useCallback, useState } from "react";
 import { useDropzone, DropzoneRootProps } from "react-dropzone";
 import { GltfFile } from "../playcanvas";
+import { createGltfWithBlobAssets } from "./utility";
 
 function isGltfFile(file: File) {
   return !!path.extname(file.name).match(/\.(gltf|glb)$/);
-}
-
-function readFile<
-  T extends "text" | "arrayBuffer" | "dataURL" | "binaryString",
-  RT = T extends "text"
-    ? string | null
-    : T extends "dataURL"
-    ? string | null
-    : T extends "binaryString"
-    ? string | null
-    : T extends "arrayBuffer"
-    ? ArrayBuffer | null
-    : never
->(blob: Blob, readAs: T) {
-  return new Promise<RT>((resolve, reject) => {
-    const reader = new FileReader();
-    reader.onabort = () => reject("File reading was aborted");
-    reader.onerror = () => reject("File reading has failed");
-    reader.onload = () => resolve((reader.result as unknown) as RT);
-    switch (readAs) {
-      case "arrayBuffer": {
-        reader.readAsArrayBuffer(blob);
-        break;
-      }
-      case "dataURL": {
-        reader.readAsDataURL(blob);
-        break;
-      }
-      case "binaryString": {
-        reader.readAsBinaryString(blob);
-        break;
-      }
-      default: {
-        reader.readAsText(blob);
-        break;
-      }
-    }
-  });
-}
-
-async function createGltfWithBlobAssets(gltf: File, assets: File[]) {
-  const gltfContent = await readFile(gltf, "text");
-  if (!gltfContent) {
-    return undefined;
-  }
-
-  const updatedGltf = assets.reduce((gltf, asset) => {
-    // Extract the guid part from the created asset url
-    const urlParts = URL.createObjectURL(asset).split("/");
-    const assetUrlString = urlParts[urlParts.length - 1];
-    // Create an expression for matching asset references in the glTF file.
-    // This expression will match full JSON strings containing the asset name.
-    // The reason for this is that we want to also replace the directory name
-    // if the asset is not placed in the same directory as the glTF file.
-    const escapedName = asset.name.replace(/\./g, "\\.");
-    const re = new RegExp(`"(?:[^"]+)"\\s*:\\s*"([^"]*${escapedName})",?`, "g");
-    // Do the replacement
-    return gltf.replace(re, (match, g1) => match.replace(g1, assetUrlString));
-  }, gltfContent);
-
-  return new Blob([updatedGltf]);
 }
 
 export const useDropModel = (
@@ -82,6 +22,7 @@ export const useDropModel = (
     async (acceptedFiles: File[]) => {
       const gltf = acceptedFiles.find(isGltfFile);
       const assets = acceptedFiles.filter(f => !isGltfFile(f));
+
       if (!gltf) {
         console.error(
           "The dropped file type is not supported. Drop a gltf or glb file.",
