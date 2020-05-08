251d9b62159b3cedc8f31b550035609adbcea797
Samuel Johansson <samuel.johansson@animech.com>
2020-05-08 14:56:06 +0200

    test(PlayCanvasViewer): refactor how XHR mocks are set up

--- src/playcanvas/__tests__/PlayCanvasViewer.spec.ts
+++ src/playcanvas/__tests__/PlayCanvasViewer.spec.ts
@@ -1,32 +1,29 @@
 import "jest";
 import xhrMock from "xhr-mock";
-import { readFile } from "fs-extra";
 import { PlayCanvasViewer } from "../PlayCanvasViewer";
+// eslint-disable-next-line
+import mockConfig from "../../../public/assets/playcanvas/config.json";
 
 describe("PlayCanvasViewer", () => {
-  let mockConfig = {};
-
-  beforeAll(async () => {
-    mockConfig = JSON.parse(
-      (await readFile("public/assets/playcanvas/config.json")).toString(),
-    );
-  });
-
   beforeEach(() => {
     xhrMock.setup();
-    xhrMock.use((...args) => {
-      // console.log(args);
-      return undefined;
-    });
+    // xhrMock.use((...args) => {
+    //   // console.log(args);
+    //   return undefined;
+    // });
     xhrMock.get(/config\.json$/, { body: JSON.stringify(mockConfig) });
   });
 
   afterEach(() => xhrMock.teardown());
 
-  it("should start and configure without crashing", async () => {
-    const canvas = document.createElement("canvas");
-    const viewer = new PlayCanvasViewer(canvas, { autoPlayAnimations: false });
-    await viewer.configure();
-    expect(viewer.initiated).toBe(true);
+  describe("Setup and teardown", () => {
+    it("should be initiated after setup", async () => {
+      const canvas = document.createElement("canvas");
+      const viewer = new PlayCanvasViewer(canvas, {
+        autoPlayAnimations: false,
+      });
+      await viewer.configure();
+      expect(viewer.initiated).toBe(true);
+    });
   });
 });
