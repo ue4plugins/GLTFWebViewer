d035bedbe22f06f90272aafea01b87c285fc0bc6
Samuel Johansson <samuel.johansson@animech.com>
2020-05-08 15:03:38 +0200

    test(PlayCanvasViewer): fix bracket enclosement error introduced in last commit

--- src/playcanvas/__tests__/PlayCanvasViewer.spec.ts
+++ src/playcanvas/__tests__/PlayCanvasViewer.spec.ts
@@ -17,9 +17,9 @@ describe("PlayCanvasViewer", () => {
     //   return undefined;
     // });
     xhrMock.get(/config\.json$/, { body: JSON.stringify(mockConfig) });
+    xhrMock.get(mockSceneUrlRegExp, { body: JSON.stringify(mockScene) });
+    xhrMock.get(/\.dds/, { body: undefined });
   });
-  xhrMock.get(mockSceneUrlRegExp, { body: JSON.stringify(mockScene) });
-  xhrMock.get(/\.dds/, { body: undefined });
 
   afterEach(() => xhrMock.teardown());
 
@@ -32,79 +32,79 @@ describe("PlayCanvasViewer", () => {
       await viewer.configure();
       expect(viewer.initiated).toBe(true);
     });
-  });
 
-  it("should have scene list after setup", async () => {
-    const canvas = document.createElement("canvas");
-    const viewer = new PlayCanvasViewer(canvas, {
-      autoPlayAnimations: false,
+    it("should have scene list after setup", async () => {
+      const canvas = document.createElement("canvas");
+      const viewer = new PlayCanvasViewer(canvas, {
+        autoPlayAnimations: false,
+      });
+      await viewer.configure();
+      expect(viewer.scenes.length).toBeGreaterThan(0);
     });
-    await viewer.configure();
-    expect(viewer.scenes.length).toBeGreaterThan(0);
-  });
 
-  it("should destroy model, scene and app on teardown", async () => {
-    // TODO
-  });
-});
-
-describe("Scene", () => {
-  it("should be able to load scene", async () => {
-    const canvas = document.createElement("canvas");
-    const viewer = new PlayCanvasViewer(canvas, {
-      autoPlayAnimations: false,
+    it("should destroy model, scene and app on teardown", async () => {
+      // TODO
     });
-    await viewer.configure();
-    expect(viewer.sceneLoaded).toBe(false);
-    expect(viewer.app.scene.root).toBe(null);
-
-    const scene = viewer.scenes.find(s => s.url === mockSceneUrl);
-    if (!scene) {
-      throw new Error("Test scene not found");
-    }
-
-    await viewer.loadScene(scene.url);
-    expect(viewer.sceneLoaded).toBe(true);
-    expect(viewer.app.scene.root).toBeInstanceOf(pc.GraphNode);
   });
 
-  it("should clean up when destroying scene", async () => {
-    //TODO
-  });
-});
+  describe("Scene", () => {
+    it("should be able to load scene", async () => {
+      const canvas = document.createElement("canvas");
+      const viewer = new PlayCanvasViewer(canvas, {
+        autoPlayAnimations: false,
+      });
+      await viewer.configure();
+      expect(viewer.sceneLoaded).toBe(false);
+      expect(viewer.app.scene.root).toBe(null);
 
-describe("Model", () => {
-  it("should be able to load model", async () => {
-    //TODO
-  });
+      const scene = viewer.scenes.find(s => s.url === mockSceneUrl);
+      if (!scene) {
+        throw new Error("Test scene not found");
+      }
 
-  it("should be able to load model from blob URL (drag-and-drop) ", async () => {
-    //TODO
-  });
+      await viewer.loadScene(scene.url);
+      expect(viewer.sceneLoaded).toBe(true);
+      expect(viewer.app.scene.root).toBeInstanceOf(pc.GraphNode);
+    });
 
-  it("should clean up when destroying model", async () => {
-    //TODO
-  });
-
-  it("should auto play animations if specified in constructor", async () => {
-    //TODO
-  });
-});
-
-describe("Camera", () => {
-  it("should have camera", async () => {
-    //TODO
+    it("should clean up when destroying scene", async () => {
+      //TODO
+    });
   });
 
-  it("should focus camera after loading model", async () => {
-    //TODO
+  describe("Model", () => {
+    it("should be able to load model", async () => {
+      //TODO
+    });
+
+    it("should be able to load model from blob URL (drag-and-drop) ", async () => {
+      //TODO
+    });
+
+    it("should clean up when destroying model", async () => {
+      //TODO
+    });
+
+    it("should auto play animations if specified in constructor", async () => {
+      //TODO
+    });
   });
 
-  it("should be able to focus on entity", async () => {
-    //TODO
-  });
+  describe("Camera", () => {
+    it("should have camera", async () => {
+      //TODO
+    });
 
-  it("should be able to reset camera placement props", async () => {
-    //TODO
+    it("should focus camera after loading model", async () => {
+      //TODO
+    });
+
+    it("should be able to focus on entity", async () => {
+      //TODO
+    });
+
+    it("should be able to reset camera placement props", async () => {
+      //TODO
+    });
   });
 });
