81c02a5c877ca5403443908ea3492cd9a252ee43
Aidin Abedi <aidin.abedi@animech.com>
2020-07-20 11:21:40 +0200

    fix: restrict regex pattern in createGltfWithBlobAssets to avoid matching filenames with the same suffix

--- src/hooks/utility/createGltfWithBlobAssets.ts
+++ src/hooks/utility/createGltfWithBlobAssets.ts
@@ -15,7 +15,10 @@ export async function createGltfWithBlobAssets(gltf: File, assets: File[]) {
     // The reason for this is that we want to also replace the directory name
     // if the asset is not placed in the same directory as the glTF file.
     const escapedName = asset.name.replace(/\./g, "\\.");
-    const re = new RegExp(`"(?:[^"]+)"\\s*:\\s*"([^"]*${escapedName})",?`, "g");
+    const re = new RegExp(
+      `"(?:[^"]+)"\\s*:\\s*"(([^"]+[\\/\\\\])?${escapedName})",?`,
+      "g",
+    );
     // Do the replacement
     return gltf.replace(re, (match, g1) => match.replace(g1, assetUrlString));
   }, gltfContent);
