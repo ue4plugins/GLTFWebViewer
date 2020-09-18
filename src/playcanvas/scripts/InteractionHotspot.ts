4001440d3dd8e906ad14e75262c2147b5f4ce0ab
Anders Gallon <anders.gallon@animech.com>
2020-09-18 10:47:10 +0200

    feat(InteractionHotspot): use pc.Picker instead of reading the depthbuffer

--- src/playcanvas/scripts/InteractionHotspot.ts
+++ src/playcanvas/scripts/InteractionHotspot.ts
@@ -3,10 +3,6 @@ import * as pc from "@animech-public/playcanvas";
 type TextureAsset = Omit<pc.Asset, "resource"> & { resource: pc.Texture };
 type OnToggleCallback = (active: boolean) => void;
 type InteractionState = "default" | "hovered" | "toggled" | "toggled-hovered";
-type Position = {
-  x: number;
-  y: number;
-};
 
 /**
  * Typings for PlayCanvas script-attributes attached to the class.
@@ -24,33 +20,37 @@ interface InteractionHotspot {
 
 const interactionHotspotScriptName = "InteractionHotspot";
 
+// NOTE: We keep a constant resolution for the picker regardless of
+// current canvas-resolution in order to prevent expensive resizing.
+const pickerWidth = 256;
+const pickerHeight = 256;
+
 class InteractionHotspot extends pc.ScriptType {
+  private static _picker: pc.Picker;
+  private static _pickerPixels: Uint8Array;
+  private static _pickerModel: pc.Model;
+  private static _pickerMaterial: pc.Material;
+  private static _instanceCount = 0;
+
   private _onToggleCallbacks: OnToggleCallback[] = [];
   private _active = false;
   private _parentElem: HTMLElement | null = null;
   private _hotspotElem: HTMLElement;
   private _hotspotImageElem: HTMLElement;
   private _cachedEntityPosition?: pc.Vec3;
-  private _lastScreenPosition?: pc.Vec3;
-  private _canvasHeight = 0;
-  private _depthPixels = new Uint8Array(4);
-  private _depthVector = new pc.Vec4();
-  private _depthDotVector = new pc.Vec4(
-    1 / (256 * 256 * 256),
-    1 / (256 * 256),
-    1 / 256,
-    1,
-  );
+  private _pickerEntity!: pc.Entity;
+
+  private _screenPosition = new pc.Vec3();
+  private _lastScreenPosition = new pc.Vec3();
+
+  private _wasVisible?: boolean;
 
   public constructor(args: { app: pc.Application; entity: pc.Entity }) {
     super(args);
 
-    this._setCanvasHeight = this._setCanvasHeight.bind(this);
     this._onClick = this._onClick.bind(this);
     this._onMouseOver = this._onMouseOver.bind(this);
     this._onMouseOut = this._onMouseOut.bind(this);
-
-    this._setCanvasHeight();
 
     this._hotspotElem = document.createElement("div");
     this._hotspotImageElem = document.createElement("div");
@@ -84,57 +84,65 @@ class InteractionHotspot extends pc.ScriptType {
   }
 
   public initialize() {
-    this._setDepthMapActive(true);
+    InteractionHotspot._onInstanceAdded();
+
     this._setImage("default");
     this._setCachedEntityPosition();
     this._setParentElem();
     this._setSize();
     this._setTransitionDuration();
+    this._addPickerEntity();
 
     this.on("attr:cacheEntityPosition", this._setCachedEntityPosition, this);
     this.on("attr:parentElementId", this._setParentElem, this);
     this.on("attr:size", this._setSize, this);
     this.on("attr:transitionDuration", this._setTransitionDuration, this);
 
-    window.addEventListener("resize", this._setCanvasHeight);
     this._hotspotElem.addEventListener("click", this._onClick);
     this._hotspotElem.addEventListener("mouseover", this._onMouseOver);
     this._hotspotElem.addEventListener("mouseout", this._onMouseOut);
 
     this.on("destroy", () => {
-      window.removeEventListener("resize", this._setCanvasHeight);
+      InteractionHotspot._onInstanceRemoved();
       this._parentElem?.removeChild(this._hotspotElem);
       this._hotspotElem.removeEventListener("click", this._onClick);
       this._hotspotElem.removeEventListener("mouseover", this._onMouseOver);
       this._hotspotElem.removeEventListener("mouseout", this._onMouseOut);
     });
+
+    this.app.on("prerender", this._onPrerender, this);
   }
 
   public update() {
-    const camera = this._getActiveCamera();
+    const camera = InteractionHotspot._getActiveCamera();
     if (!camera) {
       return;
     }
 
-    // Prevent rendering depth buffer and updating HTML element if screen pos remains unchanged
-    const screenPos = camera.worldToScreen(this._getEntityPosition());
-    if (this._lastScreenPosition?.equals(screenPos)) {
-      return;
-    }
-
-    this._lastScreenPosition = screenPos.clone();
-
-    const hitDepth = this._getPixelDepth(camera, screenPos);
-    const hidden = screenPos.z < 0 || hitDepth < screenPos.z;
-    const zIndex = Math.max(
-      Math.floor(((camera.farClip - screenPos.z) / camera.farClip) * 10000),
-      1,
+    const screenPos = this._screenPosition;
+    const isVisible = InteractionHotspot._isInstanceVisibleAtPosition(
+      this,
+      screenPos,
     );
 
-    this._hotspotElem.style.transform = `translateX(${screenPos.x}px) translateY(${screenPos.y}px)`;
-    this._hotspotElem.style.opacity = hidden ? "0" : "1";
-    this._hotspotElem.style.pointerEvents = hidden ? "none" : "auto";
-    this._hotspotElem.style.zIndex = String(zIndex);
+    // Only update position of HTML element if it has changed
+    if (!this._lastScreenPosition.equals(screenPos)) {
+      const zIndex = Math.max(
+        Math.floor(((camera.farClip - screenPos.z) / camera.farClip) * 10000),
+        1,
+      );
+
+      this._hotspotElem.style.transform = `translateX(${screenPos.x}px) translateY(${screenPos.y}px)`;
+      this._hotspotElem.style.zIndex = String(zIndex);
+    }
+
+    // Only update visibility of HTML element if it has changed
+    if (this._wasVisible !== isVisible) {
+      this._hotspotElem.style.opacity = isVisible ? "1" : "0";
+      this._hotspotElem.style.pointerEvents = isVisible ? "auto" : "none";
+    }
+
+    this._wasVisible = isVisible;
   }
 
   public onToggle(callback: OnToggleCallback) {
@@ -150,6 +158,205 @@ class InteractionHotspot extends pc.ScriptType {
       return;
     }
     this._onToggleCallbacks.splice(index, 1);
+  }
+
+  private static _onInstanceAdded() {
+    this._initialize();
+    this._instanceCount += 1;
+
+    // Start updating the picker when the first instance is added
+    if (this._instanceCount === 1) {
+      this._updatePicker();
+      this._picker.app.on("postrender", this._updatePicker, this);
+    }
+  }
+
+  private static _onInstanceRemoved() {
+    this._initialize();
+    this._instanceCount -= 1;
+
+    // Stop updating the picker when all instances have been removed
+    if (this._instanceCount === 0) {
+      this._picker.app.off("postrender", this._updatePicker, this);
+    }
+
+    // Sanity check, keep count inside valid range
+    this._instanceCount = Math.max(this._instanceCount, 0);
+  }
+
+  private static _initialize() {
+    if (this._picker) {
+      return;
+    }
+
+    const app = pc.Application.getApplication();
+    if (!app) {
+      throw new Error("No Playcanvas application running!");
+    }
+
+    this._picker = new pc.Picker(app, pickerWidth, pickerHeight);
+    this._pickerPixels = new Uint8Array(4 * pickerWidth * pickerHeight);
+
+    // Setup special material for picking hotspots
+    this._pickerMaterial = new pc.Material();
+    this._pickerMaterial.shader = new pc.Shader(app.graphicsDevice, {
+      attributes: {
+        aPosition: pc.SEMANTIC_POSITION,
+      },
+      vshader: `
+        attribute vec4 aPosition;
+
+        uniform mat4   matrix_model;
+        uniform mat4   matrix_viewProjection;
+
+        void main(void)
+        {
+            vec4 worldPosition = matrix_model * vec4(aPosition.xyz, 1);
+            gl_Position = matrix_viewProjection * worldPosition;
+            gl_PointSize = 1.0;
+        }
+    `,
+      fshader: `
+        precision mediump float;
+
+        uniform vec4 uColor;
+        varying vec4 outColor;
+
+        void main(void)
+        {
+            gl_FragColor = uColor;
+        }
+    `,
+    });
+
+    // Setup special mesh for picking hotspots
+    const mesh = new pc.Mesh(app.graphicsDevice);
+    mesh.setPositions([0, 0, 0]);
+    mesh.update(pc.PRIMITIVE_POINTS, false);
+    mesh.aabb = new pc.BoundingBox(
+      new pc.Vec3(-1, -1, -1),
+      new pc.Vec3(1, 1, 1),
+    );
+
+    const node = new pc.GraphNode();
+    const meshInstance = new pc.MeshInstance(
+      node,
+      mesh,
+      InteractionHotspot._pickerMaterial,
+    );
+
+    const model = new pc.Model();
+    model.graph = node;
+    model.meshInstances = [meshInstance];
+
+    this._pickerModel = model;
+
+    this._setPickerMaterialVisible(false);
+  }
+
+  private static _setPickerMaterialVisible(visible: boolean) {
+    this._pickerMaterial.redWrite = visible;
+    this._pickerMaterial.greenWrite = visible;
+    this._pickerMaterial.blueWrite = visible;
+    this._pickerMaterial.alphaWrite = visible;
+    this._pickerMaterial.depthWrite = visible;
+  }
+
+  private static _getActiveCamera(): pc.CameraComponent | undefined {
+    this._initialize();
+
+    const { cameras } = this._picker.app.systems.camera;
+    return cameras[cameras.length - 1];
+  }
+
+  private static _updatePicker() {
+    this._initialize();
+
+    const picker = this._picker;
+    const camera = this._getActiveCamera();
+    if (!camera) {
+      return;
+    }
+
+    const worldLayer = picker.app.scene.layers.getLayerById(pc.LAYERID_WORLD);
+
+    // Render to the picker's render-target
+    this._setPickerMaterialVisible(true);
+    picker.prepare(camera, picker.app.scene, worldLayer);
+    this._setPickerMaterialVisible(false);
+
+    // Read all pixels from the render-target into our pixel-array
+    const device = picker.app.graphicsDevice;
+    const prevRenderTarget = device.getRenderTarget();
+    device.setRenderTarget(picker.layer.renderTarget);
+    device.updateBegin();
+    device.readPixels(0, 0, pickerWidth, pickerHeight, this._pickerPixels);
+    device.updateEnd();
+    device.setRenderTarget(prevRenderTarget);
+  }
+
+  private static _isInstanceVisibleAtPosition(
+    instance: InteractionHotspot,
+    screenPosition: pc.Vec3,
+  ) {
+    const picker = this._picker;
+    const device = picker.app.graphicsDevice;
+
+    const scaleX = pickerWidth / device.width;
+    const scaleY = pickerHeight / device.height;
+
+    const pickerX = Math.round(screenPosition.x * scaleX);
+    let pickerY = Math.round(screenPosition.y * scaleY);
+
+    // Flip Y to match the way textures are stored
+    pickerY = picker.height - 1 - pickerY;
+
+    const pixels = this._pickerPixels;
+    const drawCalls = picker.layer.instances.visibleOpaque[0].list;
+
+    // To avoid issues with precision, we sample 3x3 pixels instead of just 1
+    for (let y = Math.max(pickerY - 1, 0); y < pickerY + 2; y += 1) {
+      for (let x = Math.max(pickerX - 1, 0); x < pickerX + 2; x += 1) {
+        const idx = y * picker.width + x;
+        const r = pixels[4 * idx + 0];
+        const g = pixels[4 * idx + 1];
+        const b = pixels[4 * idx + 2];
+        const index = (r << 16) | (g << 8) | b;
+
+        // White is 'no selection'
+        if (index === 0xffffff) {
+          continue;
+        }
+
+        if (drawCalls[index]?.node?.parent === instance._pickerEntity) {
+          return true;
+        }
+      }
+    }
+
+    return false;
+  }
+
+  private _addPickerEntity() {
+    this._pickerEntity = new pc.Entity();
+
+    const model = this._pickerEntity.addComponent("model");
+    model.model = InteractionHotspot._pickerModel.clone();
+
+    this.entity.addChild(this._pickerEntity);
+  }
+
+  private _onPrerender(this: InteractionHotspot) {
+    const camera = InteractionHotspot._getActiveCamera();
+    if (!camera) {
+      return;
+    }
+
+    // TODO: if screen-position is updated in the update-function, it yields
+    // unexpected results, but it works correctly when updated here.
+    // We might want to investigate why.
+    this._lastScreenPosition.copy(this._screenPosition);
+    camera.worldToScreen(this._getEntityPosition(), this._screenPosition);
   }
 
   private _onClick() {
@@ -174,11 +381,6 @@ class InteractionHotspot extends pc.ScriptType {
 
   private _getEntityPosition(): pc.Vec3 {
     return this._cachedEntityPosition ?? this.entity.getPosition();
-  }
-
-  private _getActiveCamera(): pc.CameraComponent | undefined {
-    const { cameras } = this.app.systems.camera;
-    return cameras[cameras.length - 1];
   }
 
   private _setParentElem() {
@@ -225,79 +427,6 @@ class InteractionHotspot extends pc.ScriptType {
     this._hotspotImageElem.style.backgroundImage = `url(${
       texture.resource.getSource().src
     })`;
-  }
-
-  private _setCanvasHeight() {
-    this._canvasHeight = parseInt(
-      this.app.graphicsDevice.canvas.clientHeight.toString(),
-      10,
-    );
-  }
-
-  private _setDepthMapActive(activate: boolean) {
-    const depthLayer = this.app.scene.layers.getLayerById(pc.LAYERID_DEPTH);
-    if (depthLayer.enabled !== activate) {
-      if (activate) {
-        depthLayer.incrementCounter();
-        this.app.render();
-      } else {
-        depthLayer.decrementCounter();
-      }
-    }
-  }
-
-  /**
-   * Returns the depths of the supplied screen position from the cameras depth-map.
-   *
-   * @param screenPosition A screen position to return the depth for.
-   */
-  private _getPixelDepth(
-    camera: pc.CameraComponent,
-    screenPosition: Position,
-  ): number {
-    const device = this.app.graphicsDevice;
-    const depthLayer = this.app.scene.layers.getLayerById(pc.LAYERID_DEPTH);
-    if (!depthLayer.enabled) {
-      throw new Error("No depth-map active on the camera!");
-    }
-
-    const prevRenderTarget = device.getRenderTarget();
-    const pixels = this._depthPixels;
-    const position: Position = { x: 0, y: 0 };
-
-    device.setRenderTarget(depthLayer.renderTarget);
-    device.updateBegin();
-
-    this._convertFromCanvasToDepthTexture(screenPosition, position);
-
-    device.readPixels(position.x, position.y, 1, 1, pixels);
-    this._depthVector.set(pixels[0], pixels[1], pixels[2], pixels[3]);
-    this._depthVector.scale(1 / 256); // Scale from byte to 0..1 float
-
-    const result = this._depthVector.dot(this._depthDotVector) * camera.farClip;
-
-    device.updateEnd();
-    device.setRenderTarget(prevRenderTarget);
-
-    return result;
-  }
-
-  /**
-   * Scales / converts the supplied position or rectangle from canvas-space to depth texture-space.
-   * The y-value is flipped to be bottom-up instead of top-down.
-   *
-   * @param pos Position / rectangle to convert.
-   * @param target (Optional) Target to store the result in. If omitted, the supplied value will be cloned.
-   */
-  private _convertFromCanvasToDepthTexture(
-    pos: Position,
-    target?: Position,
-  ): Position {
-    target = target || { ...pos };
-    target.x = pos.x;
-    target.y = pos.y;
-    target.y = this._canvasHeight - target.y; // Convert from top-down to bottom-up
-    return target;
   }
 }
 
